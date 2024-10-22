36e338638c9ba911e9b78ab75abf9f87f9ea58c3
Edmond-MUKUNDE <mukuned_aehit@students.vizja.pl>
2024-10-22 11:55:55 +0200

    Improved algorithm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,22 @@
 #include <vector>
 #include "Divisors.h"
+#include <cmath>
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
 	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
+	unsigned long long sqrt_number = std::sqrt(number);
+
+	for (unsigned long long i = 1; i <= sqrt_number; ++i)
 	{
 		if (number % i == 0)
+		{
 			result.push_back(i);
+			if (i != number / i)
+			{
+				result.push_back(number / i);
+			}
+		}
 	}
 	return result;
 }
